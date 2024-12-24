9c3605e3f477649aa59acf0ab014cbc323eb1377
Samuel Parfait <samuel.parfait@bbc.co.uk>
2024-12-24 15:45:15 +0000

    chore: updated ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,23 @@
-import '@/App.css';
-import reactLogo from '@/assets/react.svg';
-import viteLogo from '/vite.svg';
-import { useStore } from '@/hooks/use-store';
+import { PlusIcon } from 'lucide-react';
+
+import { useStore } from '@/src/hooks/use-store';
 
 export default function App() {
   const { count, inc } = useStore((state) => state);
 
   return (
-    <>
-      <div>
-        <a href='https://vite.dev' target='_blank'>
-          <img src={viteLogo} className='logo' alt='Vite logo' />
-        </a>
-        <a href='https://react.dev' target='_blank'>
-          <img src={reactLogo} className='logo react' alt='React logo' />
-        </a>
+    <div className='flex flex-col items-center justify-center min-h-screen bg-gradient-to-r from-blue-100 to-purple-100'>
+      <div className='p-8 bg-white rounded-lg shadow-md flex flex-col justify-center items-center'>
+        <h1 className='text-2xl font-bold mb-4 text-center'>Qty</h1>
+        <div className='text-6xl font-bold text-center mb-6'>{count}</div>
+        <button
+          onClick={inc}
+          className='flex items-center gap-2 bg-blue-600 text-white font-semibold py-2 px-4 w-full rounded-full active:scale-95'
+        >
+          <PlusIcon />
+          <span>Add to Cart</span>
+        </button>
       </div>
-      <h1>Vite + React</h1>
-      <div className='card'>
-        <button onClick={inc}>count is {count}</button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className='read-the-docs'>
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
+import '@/src/global.css';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
-import '@/global.css';
-import App from '@/App.tsx';
+import App from '@/src/App';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
